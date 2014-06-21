41953f0b411a784eb9588610f750e40b87f5dae5
Jered Philippon <jphilipp@thoughtworks.com>
2014-06-20 23:21:35 -0700

    [Jered] initial commit

---
+++ app/src/test/java/com/thoughtworks/easterEggHunt/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.easterEggHunt;
+
+
+import android.widget.TextView;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@Config(emulateSdk=18)
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void shouldTestRobolectricFramework() {
+        MainActivity mainActivity = Robolectric.buildActivity(MainActivity.class).create().get();
+
+        TextView helloText = (TextView) mainActivity.findViewById(R.id.hello);
+
+        assertThat(helloText.getText().toString(), is("Hello world!"));
+    }
+
+}
